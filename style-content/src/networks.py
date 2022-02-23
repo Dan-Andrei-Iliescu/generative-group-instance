9a8a4f6ac5cab06c1f9753ae702d3a712c3177c9
Dan-Andrei-Iliescu <iliescudanandrei@gmail.com>
2022-02-24 00:34:28 +0100

    Adjusted einsum

--- style-content/src/networks.py
+++ style-content/src/networks.py
@@ -6,15 +6,17 @@ class InstEncoder(nn.Module):
     def __init__(self, x_dim, r_dim, u_dim, v_dim, h_dim):
         super().__init__()
         self.w1 = torch.nn.init.xavier_uniform_(torch.nn.parameter.Parameter(
-            torch.randn(r_dim, u_dim, h_dim)))
+            torch.randn(r_dim, u_dim, v_dim, h_dim)))
         self.w2 = torch.nn.init.xavier_uniform_(torch.nn.parameter.Parameter(
             torch.randn(x_dim, v_dim, h_dim)))
         self.relu = torch.nn.ReLU()
 
     def forward(self, x, r, u):
-        v = torch.relu(torch.einsum(
-            'ruh,nijr,nruv->nijh', self.w1, r, u))
-        v = torch.einsum('xvh,nijx,nijh->nijv', self.w2, x, v)
+        v = torch.einsum('ruvh,nruv->nrh', self.w1, u)
+        v = torch.einsum('nijr,nrh->nijh', r, v)
+        v = torch.relu(v)
+        v = torch.einsum('xvh,nijh->nijxv', self.w2, v)
+        v = torch.einsum('nijx,nijxv->nijv', x, v)
         return v
 
 
@@ -29,8 +31,9 @@ class GroupEncoder(nn.Module):
 
     def forward(self, x, r):
         norm = x.shape[1] * x.shape[2]
-        u = torch.relu(torch.einsum(
-            'xrvh,nijx,nijr->nrvh', self.w1, x, r) / norm)
+        u = torch.einsum('nijx,nijr->nxr', x, r) / norm
+        u = torch.einsum('xrvh,nxr->nrvh', self.w1, u)
+        u = torch.relu(u)
         u = torch.einsum('ruvh,nrvh->nruv', self.w2, u)
         return u
 
@@ -39,12 +42,15 @@ class Decoder(nn.Module):
     def __init__(self, x_dim, r_dim, u_dim, v_dim, h_dim):
         super().__init__()
         self.w1 = torch.nn.init.xavier_uniform_(torch.nn.parameter.Parameter(
-            torch.randn(r_dim, u_dim, v_dim, h_dim)))
+            torch.randn(u_dim, h_dim)))
         self.w2 = torch.nn.init.xavier_uniform_(torch.nn.parameter.Parameter(
             torch.randn(x_dim, r_dim, h_dim)))
         self.relu = torch.nn.ReLU()
 
     def forward(self, r, u, v):
-        x = self.relu(torch.einsum('ruvh,nruv,nijv->nijrh', self.w1, u, v))
-        x = torch.einsum('xrh,nijr,nijrh->nijx', self.w2, r, x)
+        x = torch.einsum('nruv,nijv->nijru', u, v)
+        x = torch.einsum('nijru,nijr->nijru', x, r)
+        x = torch.einsum('uh,nijru->nijrh', self.w1, x)
+        x = torch.relu(x)
+        x = torch.einsum('xrh,nijrh->nijx', self.w2, x)
         return x
--- style-content/src/train.py
+++ style-content/src/train.py
@@ -34,28 +34,28 @@ def train(num_epochs=100):
         epoch_loss /= len(imgs)
 
         # Random images to plot
-        x = imgs[np.random.randint(0, len(imgs))]
-        y = imgs[np.random.randint(0, len(imgs))]
+        x_idx = np.random.randint(0, len(imgs))
+        y_idx = np.random.randint(0, len(imgs))
+        x = imgs[x_idx]
+        y = imgs[y_idx]
 
         x_, v_ = model.reconstruct(x)
         x_ = unpack_img(x_.detach())
         result_path = os.path.join(
-            "results", "normal", f"img_{epoch}.png")
+            "results", "normal", f"img_{epoch}_{x_idx}.png")
         write_img(x_, result_path)
 
         m = torch.randn(3, v_.shape[-1])
         v_ = torch.einsum('xv,nijv->nijx', m, v_)
         v_ = unpack_img(v_.detach())
         result_path = os.path.join(
-            "results", "normal", f"latent_{epoch}.png")
+            "results", "normal", f"latent_{epoch}_{x_idx}.png")
         write_img(v_, result_path)
 
-        if idx == 0:
-            y = x
         xy = model.translate(x, y)
         xy = unpack_img(xy.detach())
         result_path = os.path.join(
-            "results", "normal", f"trans_{epoch}.png")
+            "results", "normal", f"trans_{epoch}_{x_idx}_{y_idx}.png")
         write_img(xy, result_path)
         idx += 1
 
